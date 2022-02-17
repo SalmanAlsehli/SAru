88d8e02b86e7dd3c73eb792d9ee7b49150f0dd64
SalmanAlsehli <99890919+SalmanAlsehli@users.noreply.github.com>
2022-02-17 17:34:41 +0300

    assemply
    lab1

---
+++ folder/sample2.asm
@@ -0,0 +1,13 @@
+# sample2.asm
+	.data 0x10000100
+msg:  .asciiz "HellO COE 224"
+	.text
+main: 	li $v0, 4 # Code for syscall: print_string
+	la $a0, msg
+	syscall	
+        lb $t0,10000104
+addi $t0,-0x20
+sb $t0,0x10000104 
+syscall	
+	li $v0,10 # Code for syscall: exit
+	syscall
